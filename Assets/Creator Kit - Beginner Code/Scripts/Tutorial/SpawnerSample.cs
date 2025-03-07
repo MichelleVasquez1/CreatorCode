3a784f3fa2ecb4583b1b2b8e6b86734f01cd4332
MichelleVasquez1 <124707936+MichelleVasquez1@users.noreply.github.com>
2025-03-07 16:01:24 -0700

    Debugged

--- Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -1,43 +1,28 @@
 ﻿using UnityEngine;
-using System.Numerics;
-using Vector3 = UnityEngine.Vector3;
-using Quaternion = UnityEngine.Quaternion;
-using UnityEngine.UIElements;
-using System.Security.Cryptography.X509Certificates;
 using CreatorKitCode;
 
 public class SpawnerSample : MonoBehaviour
 {
     public GameObject ObjectToSpawn;
+
     void Start()
     {
-        LootAngle myLootAngle = new LootAngle(45);
-       
-        spawnHealth(myLootAngle.NextAngle());
-        spawnHealth(myLootAngle.NextAngle());
-        spawnHealth(myLootAngle.NextAngle());
-        spawnHealth(myLootAngle.NextAngle());
-        spawnHealth(myLootAngle.NextAngle());
+        int angle = 15;
+        Vector3 spawnPosition = transform.position;
 
-    }
-    void spawnHealth(int angle)
-    {   
-        int radius = 5;
         Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        Vector3 spawnPosition = transform.position + direction * radius;
+        spawnPosition = transform.position + direction * 2;
+        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+
+        angle = 55;
+        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+        spawnPosition = transform.position + direction * 2;
+        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+
+        angle = 95;
+        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
+        spawnPosition = transform.position + direction * 2;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
     }
 }
-public class LootAngle{
-    int angle; 
-    int step;
-    public LootAngle(int increment){
-        step = increment;
-        angle = 0;
-    }
-    public int NextAngle() {
-        int currentAngle = angle;
-        angle = Helpers.WrapAngle(angle+step);
-        return currentAngle;
-    }
-}
+
--- Assets/Scripts/ItemEffect/ItemEffect.cs
+++ Assets/Scripts/ItemEffect/ItemEffect.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 using CreatorKitCode;
 using Unity.VisualScripting;
 
-public class ItemEffect : UsableItem.UsageEffect
+public class ItemEffect : UsableItem//.UsageEffect
 {
     public int heatlhRestore = 2;
-    public override bool Use(CharacterData user){
+    public bool Use(CharacterData user){
         if(user==null) return false;
         user.Stats.ChangeHealth(heatlhRestore);
         return true;
     }
+
+    
 }
